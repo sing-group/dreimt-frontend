e54efd45314c69cadcc98bb71af808627fed6f73
Miguel Reboiro-Jato <mrjato@uvigo.es>
2019-01-08 18:26:02 +0100

    Updates copyright notice to include the new year
    The copyright notice now includes 2019 year.

--- src/app/models/signature-type.enum.ts
+++ src/app/models/signature-type.enum.ts
@@ -1,7 +1,7 @@
 /*
  * DREIMT Frontend
  *
- *  Copyright (C) 2018 - Hugo López-Fernández,
+ *  Copyright (C) 2018-2019 - Hugo López-Fernández,
  *  Daniel González-Peña, Miguel Reboiro-Jato, Kevin Troulé,
  *  Fátima Al-Sharhour and Gonzalo Gómez-López.
  *
