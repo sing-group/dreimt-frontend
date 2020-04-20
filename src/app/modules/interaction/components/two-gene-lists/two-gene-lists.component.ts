17fed2ec1c6192344d9e394cd5904aae1379da8b
Hugo López-Fernández <hlfernandez@uvigo.es>
2020-04-20 12:01:16 +0200

    Simplifies geneset loading

--- src/app/modules/interaction/components/two-gene-lists/two-gene-lists.component.ts
+++ src/app/modules/interaction/components/two-gene-lists/two-gene-lists.component.ts
@@ -15,8 +15,7 @@ export class TwoGeneListsComponent implements OnInit {
   @ViewChild('upGenes') upGenesComponent: GeneListComponent;
   @ViewChild('downGenes') downGenesComponent: GeneListComponent;
 
-  private gmtSelection: HTMLElement;
-  private gmxSelection: HTMLElement;
+  private fileSelection: HTMLElement;
 
   constructor() {
     this.upGenesChanged = new EventEmitter<string>();
@@ -24,8 +23,7 @@ export class TwoGeneListsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.gmtSelection = document.getElementById('gmt-selection');
-    this.gmxSelection = document.getElementById('gmx-selection');
+    this.fileSelection = document.getElementById('file-selection');
   }
 
   public onUpGenesChanged(genes: string): void {
@@ -37,18 +35,20 @@ export class TwoGeneListsComponent implements OnInit {
   }
 
   public getInfoTooltip(): string {
-    return 'Paste the genes in the boxes or use the buttons to import them.\n\nWhen introducing genes in the boxes, you can introduce one gene by line or one line containing all genes separated by whitespaces or tabs.';
+    return 'Paste the genes in the boxes or use the button to import them from a file.\n\nWhen introducing genes in the boxes, you can ' +
+      'introduce one gene by line or one line containing all genes separated by whitespaces or tabs.';
   }
 
-  public importGmtFile(): void {
-    this.gmtSelection.click();
+  public importFile(): void {
+    this.fileSelection.click();
   }
 
-  public importGmxFile(): void {
-    this.gmxSelection.click();
+  private static splitLine(genes: string): string[] {
+    return genes.split(/\t|,/)
+      .map(val => val.trim());
   }
 
-  onGmtChange(event) {
+  onImportFileChange(event) {
     const files = event.srcElement.files;
     if (files[0]) {
       const reader = new FileReader();
@@ -59,31 +59,32 @@ export class TwoGeneListsComponent implements OnInit {
           .map(line => line.trim())
           .filter(line => line.length > 0);
 
-        if (lines.length < 1 || lines.length > 2) {
-          throw new DreimtError(
-            'Invalid GMT file',
-            `The selected GMT file (${files[0].name}) is invalid. It must be a GMT formated file containing one or two lines.`
-          );
-        } else {
-          const firstLine = TwoGeneListsComponent.splitTsvLine(lines[0]);
-          const firstLineName = firstLine[0];
+        const genesetNamesLine = TwoGeneListsComponent.splitLine(lines[0]);
+        const firstColumnGenes = [];
+        const secondColumnGenes = [];
 
-          if (lines.length === 1) {
-            this.upGenesComponent.updateGenes(firstLine.slice(2, firstLine.length).join('\t'));
-            this.downGenesComponent.updateGenes('');
-          } else {
-            const secondLine = TwoGeneListsComponent.splitTsvLine(lines[1]);
-            const secondLineName = secondLine[0];
-
-            this.updateGeneListComponents(
-              firstLineName, firstLine.slice(2, firstLine.length),
-              secondLineName, secondLine.slice(2, firstLine.length),
-              new DreimtError(
-                'Invalid GMT file',
-                `The selected GMT file (${files[0].name}) must contain one geneset ending with '_UP' and one ending with '_DN'.`
-              )
-            );
+        for (let line = 1; line < lines.length; line++) {
+          const currentLine = TwoGeneListsComponent.splitLine(lines[line]);
+          if (currentLine[0] !== undefined) {
+            firstColumnGenes.push(currentLine[0]);
           }
+          if (currentLine[1] !== undefined) {
+            secondColumnGenes.push(currentLine[1]);
+          }
+        }
+
+        if (secondColumnGenes.length === 0) {
+          this.upGenesComponent.updateGenes(firstColumnGenes.join('\t'));
+          this.downGenesComponent.updateGenes('');
+        } else {
+          this.updateGeneListComponents(
+            genesetNamesLine[0], firstColumnGenes,
+            genesetNamesLine[1], secondColumnGenes,
+            new DreimtError(
+              'Invalid genes file',
+              `The selected genes file (${files[0].name}) must contain one geneset ending with '_UP' and one ending with '_DN'.`
+            )
+          );
         }
       };
 
@@ -109,61 +110,5 @@ export class TwoGeneListsComponent implements OnInit {
 
     this.upGenesComponent.updateGenes(upGenes);
     this.downGenesComponent.updateGenes(downGenes);
-  }
-
-  private static splitTsvLine(genes: string): string[] {
-    return genes.split('\t')
-      .map(val => val.trim());
-  }
-
-  onGmxChange(event) {
-    const files = event.srcElement.files;
-    if (files[0]) {
-      const reader = new FileReader();
-
-      reader.onload = (e) => {
-        const lines = (reader.result as string)
-          .split('\n')
-          .map(line => line.trim())
-          .filter(line => line.length > 0);
-
-        if (lines.length < 3) {
-          throw new DreimtError(
-            'Invalid GMX file',
-            `The selected GMX file (${files[0].name}) is invalid. It must be a GMX formated file.`
-          );
-        } else {
-          const genesetNamesLine = TwoGeneListsComponent.splitTsvLine(lines[0]);
-          const firstColumnGenes = [];
-          const secondColumnGenes = [];
-
-          for (let line = 2; line < lines.length; line++) {
-            const currentLine = TwoGeneListsComponent.splitTsvLine(lines[line]);
-            if (currentLine[0] !== undefined) {
-              firstColumnGenes.push(currentLine[0]);
-            }
-            if (currentLine[1] !== undefined) {
-              secondColumnGenes.push(currentLine[1]);
-            }
-          }
-
-          if (secondColumnGenes.length === 0) {
-            this.upGenesComponent.updateGenes(firstColumnGenes.join('\t'));
-            this.downGenesComponent.updateGenes('');
-          } else {
-            this.updateGeneListComponents(
-              genesetNamesLine[0], firstColumnGenes,
-              genesetNamesLine[1], secondColumnGenes,
-              new DreimtError(
-                'Invalid GMX file',
-                `The selected GMX file (${files[0].name}) must contain one geneset ending with '_UP' and one ending with '_DN'.`
-              )
-            );
-          }
-        }
-      };
-
-      reader.readAsText(files[0]);
-    }
   }
 }
